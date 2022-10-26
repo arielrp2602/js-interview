ae9e1a26890c74a41a496062c40256ab2806814e
Ariel Ramirez <louis.ramirez@rocketmiles.com>
2022-10-26 09:56:57 -0500

    First

---
+++ questions.js
@@ -0,0 +1,89 @@
+// -- PREGUNTA 1 -- //
+// Existe algún error en la forma en que se han escrito/declarado estas funciones??
+
+/* -- Primera función -- */
+// const x = func1();
+// const func1 = () => 1;
+
+/* -- Segunda función -- */
+const y = func2();
+function func2() {
+  return 2;
+}
+
+// -- PREGUNTA 2 -- //
+
+const obj1 = {
+  name: "Bugs",
+  lastName: "Bunny",
+  address: {
+    country: {
+      code: "US",
+      name: "United States",
+    },
+    city: "New York",
+    street: "The forest 1234",
+  },
+};
+
+const obj2 = { ...obj1 };
+obj2.name = "Mickey";
+obj2.lastName = "Mouse";
+obj2.address.country.code = "MEX";
+obj2.address.country.name = "México";
+obj1.address.city = "Guadalajara";
+
+/*
+    Cuáles son los valores de:
+
+    obj1.address.country.code = 
+    obj1.address.country.name = 
+    obj1.address.city = 
+    obj2.address.city = 
+*/
+
+// -- PREGUNTA 3 -- //
+
+var array1 = [1, 2, 3];
+array1 = [array1, 3];
+const [first, second, third, fourth, fifth, sixth, seventh] = [
+  ...array1,
+  ,
+  2,
+  3,
+];
+
+/* 
+    Qué valores tienen:
+
+    first:
+    second:
+    third:
+    fourth:
+    fifth:
+    sixth:
+    seventh:
+*/
+
+console.log({ first, second, third, fourth, fifth, sixth, seventh });
+
+// -- PREGUNTA 4 -- //
+
+// Componente
+// const Character = ({ name, movies }) => {
+//   // ...
+//   return (
+//     <h1>{name}</h1>
+//     // ...
+//   );
+// };
+
+// const props1 = {
+//   name: "Donald",
+// };
+
+// Retorno estos componentes
+// 1º <Character name={'Mickey'} {...props1} />
+// 2º <Character {...props1} name={'Mickey'} />
+
+// Qué nombre se va a imprimir/renderizar en cada componente
